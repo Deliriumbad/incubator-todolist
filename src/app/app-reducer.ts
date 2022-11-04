2c26bd6730f8d804d42ed296ae2ec632d44ac4eb
Vit <vit_waladzko@mail.ru>
2022-11-04 19:09:39 +0300

    Thursday/15 AXIOS, Thunk, errors, loadings(src)

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -4,30 +4,30 @@ const initialState: InitialStateType = {
 }
 
 export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-  switch (action.type) {
-      case 'APP/SET-STATUS':
-          return {...state, status: action.status}
-      case 'APP/SET-ERROR':
-          return {...state, error: action.error}
-      default:
-          return state
-  }
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
+        default:
+            return {...state}
+    }
 }
 
-export const setErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
-export const setStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
-
-type InitialStateType ={
-    //происходит ли сейчас взаимодействие с сервером
-    status: RequestStatusType,
-    //если ошибка какая-то глобальная произойдет - мы запишем текст ошибки сюда
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type InitialStateType = {
+    // происходит ли сейчас взаимодействие с сервером
+    status: RequestStatusType
+    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
     error: string | null
 }
-export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
-export type SetErrorActionType = ReturnType<typeof setErrorAC>
-export type SetStatusActionType = ReturnType<typeof setStatusAC>
+export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
 
 type ActionsType =
-    | SetErrorActionType
-    | SetStatusActionType
+    | SetAppErrorActionType
+    | SetAppStatusActionType
